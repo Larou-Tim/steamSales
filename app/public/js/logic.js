53a790c41e2b6bdb72475a797ec007caf7d7c98b
Tim LaRou <larou.tim@gmail.com>
2017-05-27 16:06:31 -0500

    notes added and loading screen

--- app/public/js/logic.js
+++ app/public/js/logic.js
@@ -1,28 +1,79 @@
-$(document).on("click", "#updateSales", function () {
 
-})
 
 
 $(document).ready(function () {
+   $('.modal').modal();
   $('.parallax').parallax();
   $(".button-collapse").sideNav();
-  //    $('.div-test').sideNav('hide');
-
+  $(document).on("submit", "#name-form", handleGameSearch);
   $(".game-tag-show, .hover-box").hover(function () {
-    // var dataString = $(this).attr("data-values");
-    // var dataArr = dataString.split(",");
-    // for(var i in dataArr) {
-    //   var chipDiv = $("<div>");
-    //   chipDiv.attr("class","chip game-tag");
-    //   chipDiv.text(dataArr[i]);
-    //   $(".hover-box").append(chipDiv)
-    // }
-    // // <div class="chip game-tag">  </div> 
-    // // console.log($(this).offset())
-    // // console.log($(this).attr("data-values"))
     $(this).children().show();
   }, function () {
     $(".hover-box").hide();
   });
+
+  $(document).on("click", "#updateSales", function () {
+    $.ajax({
+      method: "GET",
+      url: "/findSales",
+    }).done(function () {
+      location.reload();
+
+    })
+  });
+
+  function handleGameSearch() {
+    var nameInput = $("#game_name_search").val();
+
+    // Don't do anything if the name fields hasn't been filled out
+    if (!nameInput.trim().trim()) {
+      return;
+    }
+
+    var action_src = "/game/" + nameInput;
+    var your_form = document.getElementById('name-form');
+    your_form.action = action_src;
+
+  }
+
+
+  $(document).on("click", "#note-save", function () {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("game-id");
+
+    var noteText = $(this).parent().parent().find("#game-note-input").val();
+    //update current content if its there 
+    // since i'm using handlebars other option i know is to reload
+    //does not scale if someone else is adding notes to the same page
+    $(this).parents().eq(4).find("#current-game-note").text(noteText);
+
+
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/notes/" + thisId,
+      data: {
+        // Value taken from title input
+        // Value taken from note textarea
+        body: noteText
+      }
+    })
+      // With that done
+      .done(function (data) {
+        // Log the response
+
+        // Empty the notes section
+        $("#notes").empty();
+      });
+
+    // Also, remove the values entered in the input and textarea for note entry
+    updateNoteText(thisId);
+    $("#game-note-input").val("");
+
+  });
+
+  function updateNoteText(id) {
+
+  }
 
 });
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
@@ -21,17 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-//unable to set new path to views, needs to be in same directory?
-// app.engine('hbs', exphbs({extname:'handlebars', defaultLayout:'main', layoutsDir: __dirname + '/app/views'}));
 app.set("view engine", "handlebars");
 
 // Make public a static dir
 app.use(express.static("./app/public"));
 
 // Database configuration with mongoose
-// mongoose.connect("mongodb://localhost/steam_sales");
+mongoose.connect("mongodb://localhost/steam_sales");
 
-mongoose.connect("mongodb://heroku_cqmp3frb:ifa5bamtt4nu7fece1hqq584kg@ds153501.mlab.com:53501/heroku_cqmp3frb");
+// mongoose.connect("mongodb://heroku_cqmp3frb:ifa5bamtt4nu7fece1hqq584kg@ds153501.mlab.com:53501/heroku_cqmp3frb");
 var db = mongoose.connection;
 
 // Show any mongoose errors
@@ -48,43 +44,23 @@ db.once("open", function () {
 // Routes
 // ======
 
-
-/*intital steam pull. 
-    need a way to make sure game has not already been added, if game is added update price?
-    I do not want double posts
-
-    URL could be primary key?
-
-    //add this to schema
-*/
-
 //https://stackoverflow.com/questions/24122981/how-to-stop-insertion-of-duplicate-documents-in-a-mongodb-collection
 // could add index
 
-//unhandeled validation error, need to look into
-//change url to http://store.steampowered.com/search/?specials=1 (could do more than one page)
-// add rating and image?
-// lowest price seen
-
-
-
-// > a:nth-child(2)
-//
-//could be crazy double call to get additional info? not sure if it'll let me
 
 app.get("/findSales", function (req, res) {
+    res.render("load")
+    var gameCount = 0;
+    var completed = 0;
     for (var n = 1; n <= 5; n++) {
         request("http://store.steampowered.com/search/?specials=1&page=" + n, function (error, response, html) {
             var $ = cheerio.load(html);
             var result = [];
             $("#search_result_container > div:nth-child(2)").each(function (i, element) {
-                // var $this = $(this);
-                $(".search_result_row").each(function (j, inElement) {
-                    // $("a.tab_item").each(function (j, inElement) {
-
+                $(".search_result_row").each(function (j, inElement) {     
+                    gameCount++;
+                    console.log(gameCount)
                     var link = $(this).attr("href");
-
-                    // var originalPrice = $(inElement).find(".discount_original_price").text()
 
                     var name = $(inElement).find(".title").text().trim();
                     var releaseDate = $(inElement).find(".search_released").text();
@@ -112,7 +88,7 @@ app.get("/findSales", function (req, res) {
                         goodDeal = true;
                     }
 
-                    // current second request, not a good place
+                    // current second request, should add a cookie to bypass age verification
 
                     Promise.all(gameDesLinks.map(gameDescription => new Promise((resolve, reject) => {
                         request.get(gameDesLinks[0], (error, response, html) => {
@@ -122,8 +98,7 @@ app.get("/findSales", function (req, res) {
                             }
 
                             var $ = cheerio.load(html);
-                            // console.log("link is", gameDesLinks[0])
-                            // #game_highlights > div.rightcol > div > div.game_description_snippet
+
                             gameDesc = $("#game_highlights > div.rightcol > div > div.game_description_snippet").text().trim();
                             largeImage = $("#game_highlights > div.rightcol > div > div.game_header_image_ctn > img").attr("src")
                             $("#game_highlights > div.rightcol > div > div.glance_ctn_responsive_right > div > div.glance_tags.popular_tags > a").each(function (j, tagElements) {
@@ -133,9 +108,7 @@ app.get("/findSales", function (req, res) {
                                 currentTag = currentTag.replace("\r", "");
                                 currentTag = currentTag.replace("\n", "");
                                 //chracters removed to make it easy to do datavalues
-                                /*Orginally attempted to have div hoverbox for each element using handlebars to populate, 
-                                positioning easy fix is single hoverbox that is jquery developed due to realtive position issues not 
-                                sizing box correctly*/
+
                                 currentTag = currentTag.replace(" ", "-");
                                 currentTag = currentTag.replace("&", "");
                                 gameTags.push(currentTag);
@@ -143,11 +116,17 @@ app.get("/findSales", function (req, res) {
                             return resolve(res, html);
                         });
                     }))).then(function (data) {
+                        var gameReview = ''
+                        var reviewArr;
+                        if (reviews) {
+                            reviewArr = reviews.split("<")
 
-                        // console.log(gameDesc)
+                            gameReview = reviewArr[0]
+                        }
+
                         var game = {
                             game_name: name,
-                            game_reviews: reviews,
+                            game_reviews: gameReview,
                             game_image_small: image,
                             game_image_large: largeImage,
                             good_deal: goodDeal,
@@ -166,12 +145,21 @@ app.get("/findSales", function (req, res) {
 
                         var query = { game_name: game.game_name },
                             update = {
-                                original_price: game.original_price,
-                                discount_price: game.discount_price,
-                                discount_percent: game.discount_pct
+                                game_reviews: gameReview,
+                                game_image_small: image,
+                                game_image_large: largeImage,
+                                good_deal: goodDeal,
+                                game_tags: gameTags,
+                                game_description: gameDesc,
+                                release_date: releaseDate,
+                                original_price: originalPrice,
+                                discount_price: discountPrice,
+                                discount_pct: percent,
+                                game_link: link
                             },
                             options = { upsert: false };
-
+                            completed++
+                            console.log("done with:",completed)
                         if (name != "" && percent != "" && originalPrice != "" && discountPrice != "" && percent != "" && link != "") {
                             // Find the document
                             Sale.findOneAndUpdate(query, update, options, function (error, entry) {
@@ -193,57 +181,39 @@ app.get("/findSales", function (req, res) {
                             });
                         }
                     });
-                });
+                })
             });
         });
-        console.log(n + " is complete");
     }
+ 
 });
 // });
 
 
 // Serve index.handlebars to the root route.
 app.get("/", function (req, res) {
-    Sale.find({}, function (err, data) {
+    Sale.find()
+        .populate("note")
+        .exec(
+        function (err, data) {
 
-        if (err) {
-            throw err;
-        }
-        res.render("index", { deals: data });
-    });
+            if (err) {
+                throw err;
+            }
+            res.render("index", { deals: data });
+        });
 });
 
 
 //A to Z
 app.get("/alpha/:page", function (req, res) {
-    // var skipAmount = req.params.page;
-    // skipAmount = (skipAmount - 1) * 30
     var curUrl = req.url;
     sortPull("game_name", "asc", req.params.page, res, curUrl);
-    //    var perPage = 25
-    //   , page = Math.max(0, req.params.page -1);
-
-    //     Sale.find()
-    //     .limit(perPage)
-    //     .skip(perPage * page)
-    //     .sort({
-    //         game_name: 'asc'
-    //     }).exec(
-
-    //     // Sale.find({}).sort({ game_name: 1 }).skip(30).limit(skipAmount).exec(
-
-    //         function (err, data) {
-
-    //             if (err) {
-    //                 throw err;
-    //             }
-    //             res.render("index", { deals: data });
-    //         });
 });
 
 //Z to A
 app.get("/zeta/:page", function (req, res) {
-        var curUrl = req.url;
+    var curUrl = req.url;
     sortPull("game_name", "desc", req.params.page, res, curUrl);
 
     // Sale.find({}).sort({ game_name: -1 }).exec(
@@ -261,7 +231,7 @@ app.get("/zeta/:page", function (req, res) {
 
 //cheapest
 app.get("/cheapest/:page", function (req, res) {
-        var curUrl = req.url;
+    var curUrl = req.url;
     sortPull("discount_price", "asc", req.params.page, res, curUrl);
 
     // Sale.find({}).sort({ discount_price: 1 }).exec(
@@ -276,7 +246,7 @@ app.get("/cheapest/:page", function (req, res) {
 });
 //highest discount percents
 app.get("/bestdiscount/:page", function (req, res) {
-        var curUrl = req.url;
+    var curUrl = req.url;
     sortPull("discount_pct", "desc", req.params.page, res, curUrl);
 
     // Sale.find({}).sort({ discount_pct: -1 }).exec(
@@ -288,6 +258,50 @@ app.get("/bestdiscount/:page", function (req, res) {
     //         }
     //         res.render("index", { deals: data });
     //     });
+});
+
+app.get("/game/:name", function (req, res) {
+
+    var name = req.params.name;
+
+    Sale.find({ game_name: new RegExp(name, 'i') }).sort({ game_name: 1 }).exec(
+
+        function (err, data) {
+
+            if (err) {
+                throw err;
+            }
+            res.render("index", { deals: data });
+        });
+});
+
+app.post("/notes/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    var newNote = new Note(req.body);
+
+    // And save the new note the db
+    newNote.save(function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Otherwise
+        else {
+            // Use the article id to find and update it's note
+            Sale.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+                // Execute the above query
+                .exec(function (err, doc) {
+                    // Log any errors
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        // Or send the document to the browser
+                        res.send(doc);
+                    }
+                });
+        }
+    });
 });
 
 app.listen(PORT, function () {
@@ -305,6 +319,7 @@ function sortPull(field, type, pageNumber, res, currentPath) {
     var pathArr = []
     //can dynamically create the upper limit based on results in db
     for (var i = 1; i < 6; i++) {
+
         pathArr.push(
             {
                 path: "/" + curPathArr[1] + "/" + i,
@@ -318,7 +333,9 @@ function sortPull(field, type, pageNumber, res, currentPath) {
     Sale.find()
         .limit(perPage)
         .skip(perPage * page)
-        .sort(sortQuery).exec(
+        .sort(sortQuery)
+        .populate("note")
+        .exec(
 
         function (err, data) {
 
